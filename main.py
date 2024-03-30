dad6520eeb7f596293077befc3e02ead0a5bea07
Harry <ajju40959@gmail.com>
2024-03-30 17:59:21 +0000

    khjk

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from pytube import Playlist
-import os
+from pytube import YouTube
+from bs4 import BeautifulSoup
+import requests
+import re
 
-def download_videos(playlist_url, max_videos):
-    playlist = Playlist(playlist_url)
-    print(f'Downloading up to {max_videos} videos from playlist: {playlist.title}')
+def download_videos(channel_url, max_videos):
+    html = requests.get(channel_url).text
+    video_links = re.findall(r'watch\?v=\S+index', html)
+    
+    print(f'Downloading up to {max_videos} videos from channel: {channel_url}')
 
-    for url, index in zip(playlist.video_urls, range(max_videos)):
+    for video, index in zip(video_links, range(max_videos)):
         print(f'Downloading video {index + 1}...')
-        video = pytube.YouTube(url)
+        video_url = f'https://www.youtube.com/{video[:-5]}'
+        video = YouTube(video_url)
         stream = video.streams.get_highest_resolution()
         stream.download()
 
 if __name__ == "__main__":
-    playlist_url = input("Enter the YouTube playlist URL: ")
+    channel_url = input("Enter the YouTube channel URL: ")
     max_videos = int(input("Enter the maximum number of videos to download: "))
-    download_videos(playlist_url, max_videos)
+    download_videos(channel_url, max_videos)
