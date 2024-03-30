f1ba6060a400d3018d78cca29614931200cc2ee6
Harry <ajju40959@gmail.com>
2024-03-30 18:12:20 +0000

    feat: will use new library to download youtube videos.

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from pytube import YouTube
-from bs4 import BeautifulSoup
-import requests
-import re
 
-def download_videos(channel_url, max_videos):
-    html = requests.get(channel_url).text
-    video_links = re.findall(r'watch\?v=\S+index', html)
-    
-    print(f'Downloading up to {max_videos} videos from channel: {channel_url}')
+def download_youtube_video():
+    # Ask the user for the link
+    link = input("Please enter the YouTube short link: ")
 
-    for video, index in zip(video_links, range(max_videos)):
-        print(f'Downloading video {index + 1}...')
-        video_url = f'https://www.youtube.com/{video[:-5]}'
-        video = YouTube(video_url)
-        stream = video.streams.get_highest_resolution()
-        stream.download()
+    # Create a YouTube object
+    yt = YouTube(link)
 
-if __name__ == "__main__":
-    channel_url = input("Enter the YouTube channel URL: ")
-    max_videos = int(input("Enter the maximum number of videos to download: "))
-    download_videos(channel_url, max_videos)
+    # Download the highest resolution version of the video
+    yt.streams.get_highest_resolution().download()
+
+    print("Download completed!")
+
+# Call the function
+download_youtube_video()
